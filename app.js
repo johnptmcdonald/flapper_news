3147ed49b586221238c050c094e41b28e13e6e16
johnptmcdonald <johnptmcdonald@gmail.com>
2015-04-19 14:10:49 -0700

    kicking ass with ui-router and controller-as syntax

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 angular
-	.module("flapperNews", ["ngAnimate"])
-	.controller("MainController", mainController);
+	.module("flapperNews", ["ngAnimate", "ui.router"])
+	.factory('postsFactory', postsFactory)
+	.controller("MainController", mainController)
+	.config(routeFunction)
 
-	function mainController(){
+
+	routeFunction.$inject = ['$stateProvider','$urlRouterProvider'];
+	function routeFunction($stateProvider, $urlRouterProvider){
+		$stateProvider
+			.state('home', {
+				url: '/home',
+				templateUrl: 'home.html',
+				controller: 'MainController as mainCtrl'
+			});
+
+		$urlRouterProvider.otherwise('home');
+	}
+
+	mainController.$inject = ['postsFactory'];
+	function mainController(postsFactory){
+		console.log('mainController is loading');
 		var self = this;
 		self.addPost = addPost;
 		self.incrementUpvotes = incrementUpvotes;
 		self.orderByUpvotes = orderByUpvotes;
 
-		self.posts = [
-			{title: "post1", upvotes: 0, link: ""},
-			{title: "post2", upvotes: 2, link: ""},
-			{title: "post3", upvotes: 3, link: ""},
-			{title: "post4", upvotes: 4, link: ""},
-			{title: "post5", upvotes: 5, link: ""},
-			{title: "post6", upvotes: 8, link: ""},
-			{title: "post7", upvotes: 8, link: ""},
-			{title: "post8", upvotes: 10, link: ""}
-			
-		];
+		self.posts = postsFactory.posts;
+		console.log(postsFactory);
 
 		function addPost(post){
 			console.log(post)
@@ -41,7 +49,21 @@ angular
 				return obj1.upvotes - obj2.upvotes;
 			});
 		}
-		
-
-		
 	}
+
+	function postsFactory(){
+		return {
+			posts: [
+				{title: "post1", upvotes: 0, link: ""},
+				{title: "post2", upvotes: 2, link: ""},
+				{title: "post3", upvotes: 3, link: ""},
+				{title: "post4", upvotes: 4, link: ""},
+				{title: "post5", upvotes: 5, link: ""},
+				{title: "post6", upvotes: 8, link: ""},
+				{title: "post7", upvotes: 8, link: ""},
+				{title: "post8", upvotes: 10, link: ""}
+			]	
+		}		
+	}
+
+
