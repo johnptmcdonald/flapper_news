1ff08ab7b391a4879e92c256b1d2bd368090d18e
johnptmcdonald <johnptmcdonald@gmail.com>
2015-04-19 15:04:53 -0700

    implemented a posts show template with comments

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ angular
 	.module("flapperNews", ["ngAnimate", "ui.router"])
 	.factory('postsFactory', postsFactory)
 	.controller("MainController", mainController)
+	.controller("PostsController", postsController)
 	.config(routeFunction)
 
 
@@ -12,6 +13,11 @@ angular
 				url: '/home',
 				templateUrl: 'home.html',
 				controller: 'MainController as mainCtrl'
+			})
+			.state('posts', {
+				url: '/posts/{id}',
+				templateUrl: 'posts.html',
+				controller: 'PostsController as postsCtrl'
 			});
 
 		$urlRouterProvider.otherwise('home');
@@ -33,7 +39,11 @@ angular
 
 			if(post && post.title){
 				console.log("addingPost")
-				self.posts.unshift({title: post.title, upvotes: 0, link: post.link});
+				self.posts.unshift({title: post.title, upvotes: 0, link: post.link, comments: [
+				    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+				    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+				  ]});
+				console.log(self.posts[0]);
 				self.post = null;			
 			}
 		}
@@ -51,17 +61,42 @@ angular
 		}
 	}
 
+	postsController.$inject = ['postsFactory', '$stateParams'];
+	function postsController(postsFactory, $stateParams){
+		console.log("postsController is loading");
+		var self = this;
+
+		self.post = postsFactory.posts[$stateParams.id];
+		console.log(postsFactory.posts[$stateParams.id]);
+
+		self.incrementUpvotes = incrementUpvotes;
+		self.addComment = addComment;
+
+		function addComment(){
+			self.post.comments.unshift({body: self.body, author: 'A user', upvotes: 0});
+			self.body = ""
+		}
+
+		function incrementUpvotes(comment){
+			comment.upvotes++;
+			console.log("incrementing upvotes")
+		}
+	}
+
 	function postsFactory(){
 		return {
 			posts: [
-				{title: "post1", upvotes: 0, link: ""},
-				{title: "post2", upvotes: 2, link: ""},
-				{title: "post3", upvotes: 3, link: ""},
-				{title: "post4", upvotes: 4, link: ""},
-				{title: "post5", upvotes: 5, link: ""},
-				{title: "post6", upvotes: 8, link: ""},
-				{title: "post7", upvotes: 8, link: ""},
-				{title: "post8", upvotes: 10, link: ""}
+				{title: "post1", upvotes: 0, link: "link1", comments: [
+				    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+				    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+				  ]},
+				{title: "post2", upvotes: 2, link: "link2", comments: []},
+				{title: "post3", upvotes: 3, link: "link3", comments: []},
+				{title: "post4", upvotes: 4, link: "link4", comments: []},
+				{title: "post5", upvotes: 5, link: "link5", comments: []},
+				{title: "post6", upvotes: 8, link: "link6", comments: []},
+				{title: "post7", upvotes: 8, link: "link7", comments: []},
+				{title: "post8", upvotes: 10, link: "link8", comments: []}
 			]	
 		}		
 	}
